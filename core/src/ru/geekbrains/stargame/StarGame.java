f1af02141d2ced8d47f165998a664bbebd1772e0
AnnaDan <anna.v.danilina@gmail.com>
2018-01-25 22:18:24 +0300

    initial commit

---
+++ core/src/ru/geekbrains/stargame/StarGame.java
@@ -0,0 +1,37 @@
+package ru.geekbrains.stargame;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class StarGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	TextureRegion region;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("sky.jpg");
+		region = new TextureRegion(img,0,0,1024,1024);
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		//batch.draw(img, 0, 0);
+		batch.draw(region,0,0,2000,2000);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
